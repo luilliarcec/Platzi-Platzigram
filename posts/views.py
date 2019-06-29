8396f40ca815875ec89a86c782259e92165a07f0
Luis Andrés Arce C <luilliarcec@gmail.com>
2019-06-29 08:20:10 -0500

    Trabajando con template system
    Se agrego el archivo feed.html para presentar los posts
    Se modifico la presentación de los posts
    Se creo un archivo urls en el app posts
    Se modifico las urls en el proyecto para tomar urls desde apps

--- posts/views.py
+++ posts/views.py
@@ -1,43 +1,41 @@
 # Django
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 # Utilities
 from datetime import datetime
 
 posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'L. Andrés',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036',
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Via Láctea',
-        'user': 'L. Morales',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903',
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'Nuevo auditorio',
-        'user': 'D. Naula',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076',
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 
 def list_posts(request):
-    content = []
-    for post in posts:
-        content.append("""
-            <p>
-                <strong>{name}</strong>
-            </p>
-            <p>
-                <strong>{user} - <i>{timestamp}</i></strong>
-            </p>
-            <figure>
-                <img src='{picture}'/>
-            </figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'posts/feed.html', {
+        'posts': posts,
+    })
