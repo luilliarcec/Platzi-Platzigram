d7ae0d8bfaad3d2e66c8cf270c1d3fe144b9c78c
Luis Andrés Arce C <luilliarcec@gmail.com>
2019-06-29 10:06:57 -0500

    Trabajando con Modelos y PostgreSQL
    Se creo el modelo para Posts
    Se creo la migracion de ese modelo
    Se modifico el settings para trabajar con PostgreSQL

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
