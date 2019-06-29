69e91d44fba787f6b98f1803f3ea6561808a83b2
Luis Andrés Arce C <luilliarcec@gmail.com>
2019-06-29 10:55:23 -0500

    Modificando migrate posts

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,11 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     first_name = models.CharField(max_length=70)
     last_name = models.CharField(max_length=70)
+    is_admin = models.BooleanField(default=False)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
