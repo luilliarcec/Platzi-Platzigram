4852e2a78d621df7d7bc09afcd1ca73d880af757
Luis Andrés Arce C <luilliarcec@gmail.com>
2019-06-30 09:58:39 -0500

    Mejorando nuestra app Profile en el admin
    Se modificó los fieldsets a  mostrar en el detalle
    Se agrego el Profile a la vista User de django en el admin
    Se modifico el User de Django para que acepte nuestro Profile
    Se cambio el idioma del proyecto

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 # Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Models
 from users.models import Profile
@@ -8,10 +10,56 @@ from users.models import Profile
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    """Profile admin"""
+    """Vista de Profile en la vista admin de django"""
     list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
     list_display_links = ('pk', 'user')
     list_editable = ('phone_number', 'website', 'picture')
     search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name', 'phone_number')
     list_filter = ('user__is_active', 'user__is_staff', 'created_at', 'updated_at')
 
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                ('user', 'picture'),
+            )
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                'biography'
+            )
+        }),
+        ('Metadatos', {
+            'fields': (
+                ('created_at', 'updated_at'),
+            )
+        })
+    )
+
+    readonly_fields = ('created_at', 'updated_at')
+
+
+class ProfileInLine(admin.StackedInline):
+    """Vista Profile In Line que usa mi modelo Profile para agregar a User de Django en el admin"""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Agregar al User de Django en el admin el Profile In Line"""
+    inlines = (ProfileInLine,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+
+# Desregistra la Vista original de User Django del Admin
+admin.site.unregister(User)
+# Registra la vista de User modificad con nuestro Profile In Line en el admin
+admin.site.register(User, UserAdmin)
