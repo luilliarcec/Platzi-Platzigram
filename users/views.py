5bc1ff2b5deb36f128e00cdcc8708b5a48656262
Luis Andrés Arce C <luilliarcec@gmail.com>
2019-07-01 14:45:26 -0500

    Agregando signup al proyecto
    Se agrego el template signup.html
    Se agrego la url de acceso a signup
    Se agrego la funcion de signup en la vista de users 
    Se modifico el template de login
    Se modifico main css quitando el estilo de fuente

--- users/views.py
+++ users/views.py
@@ -2,10 +2,16 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+# Exceptions
+from django.db.utils import IntegrityError
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
 
 
 # Create your views here.
 def login_view(request):
+    """Login View"""
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -22,7 +28,37 @@ def login_view(request):
     return render(request, 'users/login.html')
 
 
+def signup(request):
+    """Signup View"""
+    if request.method == 'POST':
+        data = request.POST
+
+        if data['password'] != data['password_confirmation']:
+            return render(request, 'users/signup.html', {
+                'error': 'Su contraseñas no coinciden'
+            })
+
+        try:
+            user = User.objects.create_user(username=data['username'], password=data['password'])
+            user.first_name = data['first_name']
+            user.last_name = data['last_name']
+            user.email = data['email']
+            user.save()
+
+            profile = Profile(user=user)
+            profile.save()
+        except IntegrityError:
+            return render(request, 'users/signup.html', {
+                'error': 'El username especificado ya existe'
+            })
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
+
+
 @login_required
 def logout_view(request):
+    """Logout View"""
     logout(request)
     return redirect('login')
